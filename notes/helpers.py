b3e8d79e2f6235b28259ddf63b4a89ebd73e614a
Paul <cheungkuensum@gmail.com>
2023-01-26 15:34:17 +0000

    Add profile model

--- notes/helpers.py
+++ notes/helpers.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.conf import settings
 from django.shortcuts import redirect
+from django.core.exceptions import ValidationError
 
 def login_prohibited(view_function):
     def modified_view_function(request):
@@ -7,4 +10,18 @@ def login_prohibited(view_function):
             return redirect(settings.REDIRECT_URL_WHEN_LOGGED_IN)
         else:
             return view_function(request)
+
     return modified_view_function
+
+
+def calculate_age(dob):
+    today = datetime.date.today()
+    age = today.year - dob.year
+    if (dob.month, dob.day) >= (today.month, today.day):
+        age += 1
+    return age
+
+
+def validate_date(date):
+    if date > datetime.date.today():
+        raise ValidationError("The date of birth should not be in the future!")
--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from libgravatar import Gravatar
+
+from notes.helpers import validate_date
 
 
 class User(AbstractUser):
@@ -26,4 +28,30 @@ class User(AbstractUser):
         """Return a URL to a miniature version of the user's gravatar."""
         return self.gravatar(size=30)
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    age = models.IntegerField(
+        unique=False,
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(limit_value=0, message="Age cannot be a negative number"),
+                    MaxValueValidator(limit_value=180, message="Age is too high")]
+    )
+    dob = models.DateField(
+        unique=False,
+        null=True,
+        blank=True,
+        validators=[validate_date]
+    )
+
+    address = models.CharField(
+        unique=False,
+        max_length=200,
+        validators=[RegexValidator(
+            regex=r'^[\w|,|\s]*$',
+            message='Address must only contain alphanuericals, spaces or commas'
+        )],
+        null=True,
+        blank=True
+    )
