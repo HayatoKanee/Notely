29a28901139262d9460e35def69ffa92bf393f4f
Paul <cheungkuensum@gmail.com>
2023-01-28 10:23:16 +0000

    Make models for NoteBook and Page

--- notes/models.py
+++ notes/models.py
@@ -59,10 +59,15 @@ class Profile(models.Model):
 
 class Folder(models.Model):
     user = models.ForeignKey(User, related_name="folders", on_delete=models.CASCADE)
-    name = models.CharField(
-        max_length=10
-    )
+    name = models.CharField(max_length=10)
 
 
+class NoteBook(models.Model):
+    user = models.ForeignKey(User, related_name="notebooks", on_delete=models.CASCADE)
+    folder = models.ForeignKey(Folder, related_name="notebooks", on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=10)
 
 
+class Page(models.Model):
+    notebook = models.ForeignKey(NoteBook, related_name="pages", on_delete=models.CASCADE)
+    drawing = models.TextField(blank=True)
