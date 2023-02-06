fa2dff7b649a500d8dbb45669245650cfd2016b3
Paul <cheungkuensum@gmail.com>
2023-02-06 23:10:51 +0000

    Improve the line number showing in the code editor

--- notes/static/javascript/codeEditor.js
+++ notes/static/javascript/codeEditor.js
@@ -1,10 +1,10 @@
+const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    mode: "python",
+  });
 $("#showCodeEditor").click(function () {
   $(".modal-dialog").draggable({
     "handle": ".modal-header"
   });
+  editor.refresh()
 });
-
-CodeMirror.fromTextArea(document.getElementById("code"), {
-    lineNumbers: true,
-    mode: "python"
-  });
