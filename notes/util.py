7bca2782cbccb59bc82d5606ae0baaa363cb061b
Ryan Tsoi <sanwingtsoi@gmail.com>
2023-02-07 15:07:12 +0000

    fix bugs at .utils

--- notes/util.py
+++ notes/util.py
@@ -12,33 +12,28 @@ class EventCalendar(HTMLCalendar):
         self.events = events
         super(EventCalendar, self).__init__()
 
-    def formatday(self, day, events):
-        events_today = events.filter(start_time__day=day)
-        if (events_today != None):
-            return daytd(day,events_today)
-        return '<td></td>'
-
-    def formatweek(self, theWeek, events):
-        week = ''
-        for day in theWeek:
-            week += self.formatday(day, events)
-        return f'<tr> {week} </tr>'
-
-    def formatmonth(self, withyear=True):
-        events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
-
-        calendar = f'<table border="5" cellpadding="0" cellspacing="0" class="calendar">\n'
-        calendar += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
-        calendar += f'{self.formatweekheader()}\n'
-        for week in self.monthdays2calendar(self.year, self.month):
-            calendar += f'{self.formatweek(week, self.events)}\n'
-        calendar +=f'</table>'
-        return calendar
-
-    def daytd(self, day , events):
+    def formatday(self, day):
         tdContent = ""
-        for event in events:
+        for event in self.events.filter(start_time__day=day):
                 tdContent += f'<li> {event.title} </li>'
         if (day == datetime.now().day):      
             return f"<td bgcolor='MediumSeaGreen'><span class='date'>{day}</span><ul> {tdContent} </ul></td>"
-        return f"<td><span class='date'>{day}</span><ul> {tdContent} </ul></td>"
+        if (day != 0):
+            return f"<td><span class='date'>{day}</span><ul> {tdContent} </ul></td>"
+        return '<td></td>'
+
+    def formatweek(self, theWeek):
+        week = ''
+        for day,weekday in theWeek:
+            week += self.formatday(day)
+        return f'<tr> {week} </tr>'
+
+    def formatmonth(self, withyear=True):
+        cal = f'<table border="5" cellpadding="0" cellspacing="0" class="calendar">\n'
+        cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
+        cal += f'{self.formatweekheader()}\n'
+        for week in self.monthdays2calendar(self.year, self.month):
+            cal += f'{self.formatweek(week)}\n'
+        cal +=f'</table>'
+        return cal
+
