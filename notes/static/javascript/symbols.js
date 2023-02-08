3e69bf9b6fc9efad9165206060dbfbf6c7f9614c
Paul <cheungkuensum@gmail.com>
2023-02-08 14:38:42 +0000

    Improve the symbol inserting function

--- notes/static/javascript/symbols.js
+++ notes/static/javascript/symbols.js
@@ -1,27 +1,36 @@
+const symbol = document.getElementById("showSymbols");
+symbol.disabled = true;
 $("#showSymbols").click(function (){
      $(".modal-dialog").draggable({
     "handle": ".modal-header"
   });
 });
+canvas.on('selection:created', function (){
+   if(canvas.getActiveObject().type !== 'activeSelection'){
+       if(canvas.getActiveObject().get('type') == 'i-text'){
+            symbol.disabled = false;
+        }
+   }
+});
+canvas.on('selection:updated', function (){
+   if(canvas.getActiveObject().type !== 'activeSelection'){
+       if(canvas.getActiveObject().get('type') == 'i-text'){
+            symbol.disabled = false;
+            return;
+        }
+   }
+   symbol.disabled = true;
+});
+
+canvas.on('selection:cleared', function (){
+    symbol.disabled = true;
+});
 
 $('.key').click(function (){
-    t = new fabric.Text($(this).text());
-    t.set({
-        left: canvas.width/2,
-        top: canvas.height/2
-    })
-    
-
-    console.log("Text", canvas.getActiveObject().get('type'))
-
-    if(canvas.getActiveObject().get('type') == 'i-text'){
-      var itext =  canvas.getActiveObject()
-      console.log("test123", itext)
-      // .get('text');
-      itext.insertChars($(this).text(), [{ fill: "green", fontStyle: "bold" }], 6,7);
-    }
-
-    canvas.add(itext);
+    let itext =  canvas.getActiveObject()
+    itext.insertChars($(this).text(), [{fill: "green",
+        fontStyle: "bold"}], itext.selectionStart);
+    canvas.renderAll()
     
 });
 
