5afd249def0c449d5ce8c89e86aa32b1c8e0c3f6
Ryan Tsoi <sanwingtsoi@gmail.com>
2023-02-19 03:15:17 +0000

    add urlrouter for asgi , add routing , adjust signal consumer add js for calendar_tab

--- notes/consumer.py
+++ notes/consumer.py
@@ -1,5 +1,5 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-import asyncio
+import asyncio , json
 
 class ReminderConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -12,10 +12,9 @@ class ReminderConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(f"user_{self.scope['user'].id}", self.channel_name)
 
     async def receive(self, text_data):
-        # Not used for this example
         pass
 
-    async def show_reminder(self, event):
+    async def schedule_reminder(self, event):
         # Sleep for the specified delay and then send a "show_notification" message to the user's browser
         await asyncio.sleep(event["delay"])
         await self.send(text_data=json.dumps({"type": "show_notification", "message": event["message"]}))
