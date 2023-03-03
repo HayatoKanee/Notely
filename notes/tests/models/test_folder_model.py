4cab1dd4ff4928cd961ca723125d6075c50c8f5d
Paul <cheungkuensum@gmail.com>
2023-03-03 15:46:37 +0000

    Add some test cases for the folder model

--- notes/tests/models/test_folder_model.py
+++ notes/tests/models/test_folder_model.py
@@ -34,6 +34,30 @@ class FolderModelTestCase(TestCase):
         self.folder.folder_name = Folder.objects.get(pk=2).folder_name
         self._assert_folder_is_valid()
 
+    def test_folder_must_have_a_user(self):
+        self.folder.user = None
+        self._assert_folder_is_invalid()
+
+    def test_folder_get_type(self):
+        self.assertEqual(self.folder.get_type(), 'Folder')
+
+    def test_folder_can_have_a_parent(self):
+        parent = Folder.objects.create(user=User.objects.get(id=1),
+                                       folder_name='my_folder')
+        self.folder.parent = parent
+        self._assert_folder_is_valid()
+
+    def test_folder_can_have_no_parent(self):
+        self.folder.parent = None
+        self._assert_folder_is_valid()
+
+    def test_folder_get_path(self):
+        self.folder.parent = Folder.objects.create(user=User.objects.get(id=1),
+                                                   folder_name='my_folder')
+        path = self.folder.get_path()
+        self.assertEqual(path[0], self.folder.parent)
+        self.assertEqual(path[1], self.folder)
+
     # Validation (helpers)
     def _assert_folder_is_valid(self):
         try:
