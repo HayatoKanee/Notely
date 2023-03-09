acf6c2cc1af0691c66513c94adda91ef74a34dd9
Paul <cheungkuensum@gmail.com>
2023-03-09 14:13:30 +0000

    Show the page number when link

--- notes/static/javascript/note-taking/linkPage.js
+++ notes/static/javascript/note-taking/linkPage.js
@@ -1,45 +1,65 @@
 const link = document.getElementById("linkPage");
 link.disabled = true;
-$('#linkPage').click(function (){
+$('#linkPage').click(function () {
     const selection = canvas.getActiveObject();
-     $('a').click(function (e){
-         e.preventDefault();
-         const tag = $(this);
-      const underline = new fabric.Path('M' + selection.left + ',' + (selection.top + selection.height) + ' L' + (selection.left + selection.width) + ',' + (selection.top + selection.height));
-underline.set({
-  stroke: 'blue',
-  strokeWidth: 1,
-  selectable: false
-});
-canvas.remove(selection);
+    $('a').click(function (e) {
+        e.preventDefault();
+        const tag = $(this);
+        const underline = new fabric.Path('M' + selection.left + ',' + (selection.top + selection.height) + ' L' + (selection.left + selection.width) + ',' + (selection.top + selection.height));
+        underline.set({
+            stroke: 'blue',
+            strokeWidth: 1,
+            selectable: false
+        });
+        const text = new fabric.Text('page ' + tag.attr('data-page-num'), {
+            left: selection.left + selection.width + 10,
+            top: selection.top,
+            fontFamily: 'Arial',
+            fontSize: 12,
+            fill: 'black',
+            visible: false
+        });
+        canvas.remove(selection);
 
-// combine the rectangle and underline into a single fabric object
-const obj = new fabric.Group([selection, underline]);
-        obj.on('mousedown', function (e){
-            if(currentMode=='select') {
+        const obj = new fabric.Group([selection, underline, text]);
+        obj.on('mousedown', function (e) {
+            if (currentMode === 'select') {
                 window.location.replace(tag.attr('href'));
             }
         });
-         obj.set({
-             link:tag.attr('href')
-         });
-canvas.add(obj);
-         canvas.discardActiveObject();
-         canvas.renderAll();
-         $('#closeBtn').click();
+        obj.set({
+            link: tag.attr('href')
+        });
+        obj.on('mouseover', function () {
+            obj.item(2).set({
+                visible: true
+            });
+            canvas.renderAll();
+        });
+
+        obj.on('mouseout', function () {
+            obj.item(2).set({
+                visible: false
+            });
+            canvas.renderAll();
+        });
+        canvas.add(obj);
+        canvas.discardActiveObject();
+        canvas.renderAll();
+        $('#closeBtn').click();
     });
 });
-$('#sideBtn').click(function (){
+$('#sideBtn').click(function () {
     $("a").unbind("click");
 });
-canvas.on('selection:created', function (){
-   if(canvas.getActiveObject().type !== 'activeSelection'){
-       link.disabled = false;
-   }
+canvas.on('selection:created', function () {
+    if (canvas.getActiveObject().type !== 'activeSelection') {
+        link.disabled = false;
+    }
 
 });
 
-canvas.on('selection:cleared', function (){
+canvas.on('selection:cleared', function () {
     link.disabled = true;
 });
 
--- notes/static/javascript/note-taking/sync_page.js
+++ notes/static/javascript/note-taking/sync_page.js
@@ -1,35 +1,49 @@
-
-  const objectChangedCallback = function (){
-      // send a message to the channel using websockets
-      const message = {
+const objectChangedCallback = function () {
+    // send a message to the channel using websockets
+    const message = {
         type: 'update_canvas',
         data: canvas.toDatalessJSON(['link'])
-      };
-      socket.send(JSON.stringify(message));
-  }
-  canvas.on('object:added', objectChangedCallback);
-      canvas.on('object:modified', objectChangedCallback);
-      canvas.on('object:removed', objectChangedCallback);
-    socket.onmessage = function(event) {
-        const data = JSON.parse(event.data);
-        if (data.type === "update_canvas") {
-              canvas.off('object:added', objectChangedCallback);
-      canvas.off('object:modified', objectChangedCallback);
-      canvas.off('object:removed', objectChangedCallback);
-            canvas.loadFromJSON(data.data, function(){
-                canvas.getObjects().forEach(function(obj) {
+    };
+    socket.send(JSON.stringify(message));
+}
+
+function set_send_message() {
+    canvas.on('object:added', objectChangedCallback);
+    canvas.on('object:modified', objectChangedCallback);
+    canvas.on('object:removed', objectChangedCallback);
+}
+set_send_message();
+socket.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+    if (data.type === "update_canvas") {
+        canvas.off('object:added', objectChangedCallback);
+        canvas.off('object:modified', objectChangedCallback);
+        canvas.off('object:removed', objectChangedCallback);
+        canvas.loadFromJSON(data.data, function () {
+            canvas.getObjects().forEach(function (obj) {
                 if (obj.link) {
-                  obj.on('mousedown', function(e) {
-                      if(currentMode=='select') {
-                          window.location.href = this.link;
-                      }
-                  });
+                    obj.on('mousedown', function (e) {
+                        if (currentMode == 'select') {
+                            window.location.href = this.link;
+                        }
+                    });
+                    obj.on('mouseover', function () {
+                        obj.item(2).set({
+                            visible: true
+                        });
+                        canvas.renderAll();
+                    });
+
+                    obj.on('mouseout', function () {
+                        obj.item(2).set({
+                            visible: false
+                        });
+                        canvas.renderAll();
+                    });
                 }
-              });
-                canvas.renderAll();
-                canvas.on('object:added', objectChangedCallback);
-      canvas.on('object:modified', objectChangedCallback);
-      canvas.on('object:removed', objectChangedCallback);
             });
-        }
+            canvas.renderAll();
+            set_send_message();
+        });
     }
+}
