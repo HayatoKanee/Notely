e868bc3b1272d59626627577010db8f625220b7d
heliip812 <heliip812@gmail.com>
2023-03-18 17:56:31 +0000

    add tests for get option view notebook, folder, event

--- notes/views.py
+++ notes/views.py
@@ -484,7 +484,10 @@ def share_page(request, page_id):
 def get_options_notebook(request, notebook_id):
     try:
         notebook = Notebook.objects.get(id=notebook_id)
-        return JsonResponse(get_options(notebook, 'dg_view_notebook'), safe=False)
+        return JsonResponse({
+            'status': 'success',
+            'options': get_options(notebook, 'dg_view_notebook')
+        }, safe=False)
     except Notebook.DoesNotExist:
         return JsonResponse({'status': 'fail'})
 
@@ -493,7 +496,10 @@ def get_options_notebook(request, notebook_id):
 def get_options_folder(request, folder_id):
     try:
         folder = Folder.objects.get(id=folder_id)
-        return JsonResponse(get_options(folder, 'dg_view_folder'), safe=False)
+        return JsonResponse({
+            'status': 'success',
+            'options': get_options(folder, 'dg_view_folder')
+        }, safe=False)
     except Notebook.DoesNotExist:
         return JsonResponse({'status': 'fail'})
 
@@ -564,7 +570,10 @@ def share_event(request, event_id):
 def get_options_event(request, event_id):
     try:
         event = Event.objects.get(id=event_id)
-        return JsonResponse(get_options(event, 'dg_view_event'), safe=False)
+        return JsonResponse({
+            'status': 'success',
+            'options': get_options(event, 'dg_view_event')
+        }, safe=False)
     except Event.DoesNotExist:
         return JsonResponse({'status': 'fail'})
 
