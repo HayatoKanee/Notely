13b9e765230d8cf8ce4f55fe6c33e32a05923d75
Paul <cheungkuensum@gmail.com>
2023-03-20 19:20:28 +0000

    Allow user to download as pdf

--- notes/static/javascript/download.js
+++ notes/static/javascript/download.js
@@ -1,5 +1,21 @@
-$("#downloadAsPic").click(function(){
-	$("#canvas").get(0).toBlob(function(blob){
-		saveAs(blob, "notely_notebook.png");
-	});
+$("#downloadAsPic").click(function () {
+    $("#canvas").get(0).toBlob(function (blob) {
+        saveAs(blob, "notely_notebook.png");
+    });
+});
+$("#downloadAsPdf").click(function () {
+    // const imgData = canvas.toDataURL("image/jpeg");
+
+    $("#canvas").get(0).toBlob(function (blob) {
+        const url = window.URL || window.webkitURL;
+        const img = new Image();
+        const imgSrc = url.createObjectURL(blob);
+        img.src = imgSrc;
+
+        img.onload = function () {
+            const pdf = new jsPDF('l', 'px', [img.width, img.height]);
+            pdf.addImage(img, 0, 0, img.width, img.height);
+            pdf.save('download.pdf');
+        }
+    });
 });
